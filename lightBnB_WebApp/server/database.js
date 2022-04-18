c26f23b8b322a18976bcfab199c2d99cc2948307
Manuel Casanova <manucasanova@hotmail.com>
2022-04-18 10:00:53 -0700

    refactor addUser function to be able to insert users tothe database from the frontend

--- lightBnB_WebApp/server/database.js
+++ lightBnB_WebApp/server/database.js
@@ -66,11 +66,21 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+// const addUser =  function(user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// }
+
+const addUser = function (user) {
+  return pool.query(`INSERT INTO users (name, email, password)
+VALUES($1, $2, $3)
+RETURNING *;
+`, [user.name, user.email, user.password])
+    .then(res => {
+      return res.rows[0];
+    })
 }
 exports.addUser = addUser;
 
