e6e65dafb9d83a09f159fe556dbd2df6b7a08b68
Manuel Casanova <manucasanova@hotmail.com>
2022-04-19 12:50:37 -0700

    continue to refactor get all properties function. Now able to filter by minimum and maximum price

--- lightBnB_WebApp/server/database.js
+++ lightBnB_WebApp/server/database.js
@@ -147,8 +147,17 @@ exports.getAllReservations = getAllReservations;
     }
   }
 
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += `AND cost_per_night >= $${queryParams.length} `;
+    
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `AND cost_per_night <= $${queryParams.length} `;
+  }
 
-  // 4
+
   queryParams.push(limit);
   queryString += `
   GROUP BY properties.id
