a5a92f54fc2f7703dd82c7d452662dcd67e4d546
Manuel Casanova <manucasanova@hotmail.com>
2022-04-21 08:46:40 -0700

    added cost per night to the addProperty funciton

--- lightBnB_WebApp/server/database.js
+++ lightBnB_WebApp/server/database.js
@@ -166,6 +166,7 @@ exports.getAllProperties = getAllProperties;
       property['description'],
       property['thumbnail_photo_url'],
       property['cover_photo_url'],
+      property['cost_per_night'],
       property['street'],
       property['city'],
       property['province'],
@@ -177,8 +178,8 @@ exports.getAllProperties = getAllProperties;
     ];
   
     const queryString = `
-    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+    INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
     RETURNING *;`
     return pool.query(queryString, queryParams).then((res) => res.rows);
   }
